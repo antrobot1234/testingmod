a04c94a287e39a1897d84489a734e33f419c0052
antrobot1234 <antrobot1234@gmail.com>
2019-05-28 16:44:07 -0500

    added a single init function for subscribing smelting recipes

--- src/main/java/mod/antrobot/anttm/Main.java
+++ src/main/java/mod/antrobot/anttm/Main.java
@@ -1,9 +1,16 @@
 package mod.antrobot.anttm;
+import mod.antrobot.anttm.subscribers.SmeltingSubscriber;
 import mod.antrobot.anttm.util.ModReference;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 
 
 @Mod(modid = ModReference.ID,name = ModReference.NAME,version = ModReference.VERSION)
 public class Main {
     public static Main instance;
+    @Mod.EventHandler
+    public static void init(FMLInitializationEvent event){
+        SmeltingSubscriber.registerSmeltingRecipes();
+    }
 }
