298836a21fb87f74266c03d56d0e1bf94369275f
antrobot1234 <antrobot1234@gmail.com>
2019-05-30 12:19:21 -0500

    finished(?) basic IO interface

--- src/main/java/mod/antrobot/anttm/capabilities/basicio/IBasicIO.java
+++ src/main/java/mod/antrobot/anttm/capabilities/basicio/IBasicIO.java
@@ -1,4 +1,17 @@
 package mod.antrobot.anttm.capabilities.basicio;
 
 public interface IBasicIO {
+    String input = "null";
+    String output = "null";
+
+    String getInput();
+    void setInput(String i);
+
+    String getOutput();
+    void setOutput(String i);
+
+    int calculateInput(int slot);
+    int calculateOutput(int slot);
+
+    boolean isValid(int slot);
 }
