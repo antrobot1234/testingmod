77d7edd48554dd196c335e28c9a3a5a725a7be2b
antrobot1234 <antrobot1234@gmail.com>
2019-05-30 14:09:37 -0500

    Basic IO factory

--- src/main/java/mod/antrobot/anttm/Main.java
+++ src/main/java/mod/antrobot/anttm/Main.java
@@ -1,15 +1,27 @@
 package mod.antrobot.anttm;
+import mod.antrobot.anttm.capabilities.basicio.BasicFactory;
+import mod.antrobot.anttm.capabilities.basicio.BasicStorage;
+import mod.antrobot.anttm.capabilities.basicio.IBasicIO;
 import mod.antrobot.anttm.subscribers.SmeltingSubscriber;
 import mod.antrobot.anttm.util.ModReference;
+import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 
 @Mod(modid = ModReference.ID,name = ModReference.NAME,version = ModReference.VERSION)
 public class Main {
     public static Main instance;
     @Mod.EventHandler
+    public static void preInit(FMLPreInitializationEvent event){
+        CapabilityManager.INSTANCE.register(IBasicIO.class,new BasicStorage(), new BasicFactory());
+    }
+
+    @Mod.EventHandler
     public static void init(FMLInitializationEvent event){
         SmeltingSubscriber.registerSmeltingRecipes();
+
     }
 }
---
+++ src/main/java/mod/antrobot/anttm/capabilities/basicio/BasicFactory.java
@@ -0,0 +1,92 @@
+package mod.antrobot.anttm.capabilities.basicio;
+
+import java.util.concurrent.Callable;
+
+public class BasicFactory implements Callable<IBasicIO> {
+    @Override
+    public IBasicIO call() throws Exception {
+        return new IBasicIO() {
+            String input = "left";
+            boolean output = true;
+
+            @Override
+            public boolean validInput(String i){
+                String[] possibilities = "left right up down".split(" ");
+                for(String s: possibilities){
+                    if(s.equals(i))return true;
+                }
+                return false;
+            }
+            @Override
+            public String getInput() {
+                return this.input;
+            }
+            @Override
+            public void setInput(String i){
+                if(!validInput(i))throw new IllegalArgumentException("input is not one of the four valid responses: up, down, left, right");
+                input = i.toLowerCase();
+            }
+
+            @Override
+            public boolean getOutput(){
+                return output;
+            }
+            @Override
+            public void setOutput(boolean i) {
+                output = i;
+            }
+
+            @Override
+            public int calcLeft(int slot) {
+                if(slot%9==0)return -1;
+                return slot-1;
+            }
+            @Override
+            public int calcRight(int slot) {
+                if(slot%9==8)return -1;
+                return slot+1;
+            }
+            @Override
+            public int calcUp(int slot) {
+                if(slot/10==3)return -1;
+                return slot+9;
+            }
+            @Override
+            public int calcDown(int slot) {
+                if(slot/10==0)return -1;
+                return slot-9;
+            }
+
+            @Override
+            public int calcInput(int slot) {
+                if(input.equals("left"))return calcLeft(slot);
+                if(input.equals("right"))return calcRight(slot);
+                if(input.equals("up"))return calcUp(slot);
+                if(input.equals("down"))return calcDown(slot);
+                return -1;
+            }
+            @Override
+            public int calcOutput(int slot) {
+                if(!output)return -1;
+                if(input.equals("left"))return calcRight(slot);
+                if(input.equals("right"))return calcLeft(slot);
+                if(input.equals("up"))return calcDown(slot);
+                if(input.equals("down"))return calcUp(slot);
+                return -1;
+            }
+
+            @Override
+            public boolean isValid(int slot) {
+                return calcInput(slot)==-1|| calcOutput(slot)==-1;
+            }
+
+            @Override
+            public void cycleInput(){
+                if(input.equals("left"))input = "up";
+                else if(input.equals("right"))input = "down";
+                else if(input.equals("up"))input = "right";
+                else if(input.equals("down"))input = "left";
+            }
+        };
+    }
+}
