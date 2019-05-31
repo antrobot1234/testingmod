922067a360d62476df248c5a41d5e57727306f9f
antrobot1234 <antrobot1234@gmail.com>
2019-05-31 13:31:36 -0500

    made extender an itemTooltip

--- src/main/java/mod/antrobot/anttm/items/ItemTooltip.java
+++ src/main/java/mod/antrobot/anttm/items/ItemTooltip.java
@@ -1,21 +1,24 @@
 package mod.antrobot.anttm.items;
 
-import com.sun.istack.internal.Nullable;
+
 import mod.antrobot.anttm.util.ModReference;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.minecraft.client.resources.I18n;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.util.List;
 
 public class ItemTooltip extends Item {
     public ItemTooltip(String tip){
         tipName = tip;}
-    private static String tipName;
+    private String tipName;
     @Override
-    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn){
+    @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack stack, World worldIn, List<String> tooltip, ITooltipFlag flagIn){
         tooltip.add(I18n.format(ModReference.ID+".tooltip."+tipName));
     }
 }
