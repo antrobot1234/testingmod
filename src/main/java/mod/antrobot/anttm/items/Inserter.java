b429544b2b7b17f42df46c72603d2ca3d48ec950
antrobot1234 <antrobot1234@gmail.com>
2019-05-31 14:58:28 -0500

    implement BasicProvider

--- src/main/java/mod/antrobot/anttm/items/Inserter.java
+++ src/main/java/mod/antrobot/anttm/items/Inserter.java
@@ -1,7 +1,9 @@
 package mod.antrobot.anttm.items;
 
 import mod.antrobot.anttm.capabilities.basicio.BasicFactory;
+import mod.antrobot.anttm.capabilities.basicio.BasicProvider;
 import mod.antrobot.anttm.capabilities.basicio.IBasicIO;
+import mod.antrobot.anttm.util.ModReference;
 import net.minecraft.client.renderer.ItemMeshDefinition;
 import net.minecraft.client.renderer.block.model.ModelBakery;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
@@ -21,13 +23,13 @@ import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityInject;
 import net.minecraftforge.common.capabilities.ICapabilitySerializable;
-import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class Inserter extends ItemTooltip{
-    @GameRegistry.ObjectHolder("anttm:extender")
-    public static final Item extender = null;
+    @ObjectHolder(ModReference.ID+":extender")
+    private static final Item extender = null;
 
     public Inserter(String tooltip) {
         super(tooltip);
@@ -45,7 +47,6 @@ public class Inserter extends ItemTooltip{
         IBasicIO io = item.getCapability(IO_CAPABILITY,null);
         if(io == null)return output;
         io.cycleInput();
-        playerIn.sendStatusMessage(new TextComponentString("pulling from: "+io.getInput()),true);
         return output;
     }
     @Override
@@ -63,26 +64,25 @@ public class Inserter extends ItemTooltip{
 
         ItemStack inStack = inventory.getStackInSlot(inSlot);
         if (inStack.isEmpty()) return;
-        if (inStack.getItem()==this)return;
+        if (inStack.getItem()==this||inStack.getItem()==extender)return;
         int outSlot = io.calcOutput(slot);
         ItemStack outStack = inventory.getStackInSlot(outSlot);
-        if(outStack.getItem()==this){
-            forward(inventory,inStack,outStack,outSlot,1);
+        if(outStack.getItem()==extender){
+            forward(inventory,stack,inStack,outStack,outSlot,1);
         }
         else{
         insert(inventory,inStack,outStack,outSlot);
         }
     }
-    private void forward(InventoryPlayer inventory,ItemStack inStack, ItemStack currentWorker,int currentSlot,int count){
+    private void forward(InventoryPlayer inventory,ItemStack firstWorker,ItemStack inStack, ItemStack currentWorker,int currentSlot,int count){
         if(count >= 10)return;
-        if(inStack==currentWorker)return;
-        IBasicIO io = currentWorker.getCapability(IO_CAPABILITY,null);
+        IBasicIO io = firstWorker.getCapability(IO_CAPABILITY,null);
         if(io == null)return;
         if(io.isInvalid(currentSlot))return;
         int outSlot = io.calcOutput(currentSlot);
         ItemStack outStack = inventory.getStackInSlot(outSlot);
-        if(outStack != currentWorker && outStack.getItem()==this){
-            forward(inventory,inStack,outStack,outSlot,count+1);
+        if(outStack.getItem()==extender){
+            forward(inventory,firstWorker,inStack,outStack,outSlot,count+1);
             //TODO see and fix why the fuck this crashed the game with an infinte loop (i mean yeah recursion but seriously)
         }
         else{
@@ -107,33 +107,7 @@ public class Inserter extends ItemTooltip{
     @Override
     public ICapabilitySerializable<NBTTagCompound> initCapabilities(ItemStack stack, NBTTagCompound nbt)
     {
-        IBasicIO instance = IO_CAPABILITY.getDefaultInstance();
-        return new ICapabilitySerializable<NBTTagCompound>() {
-            @Override
-            public NBTTagCompound serializeNBT(){
-                NBTTagCompound nbt = new NBTTagCompound();
-                nbt.setString("input",instance.getInput().toString());
-                nbt.setBoolean("output",instance.getOutput());
-                return nbt;
-            }
-            @Override
-            public void deserializeNBT(NBTTagCompound nbt){
-                instance.setInput(nbt.getString("input"));
-                instance.setOutput(nbt.getBoolean("output"));
-            }
-
-            @Override
-            public boolean hasCapability(Capability<?> capability, EnumFacing facing) {
-                if(capability == IO_CAPABILITY)return true;
-                return false;
-            }
-
-            @Override
-            public <T> T getCapability(Capability<T> capability, EnumFacing facing) {
-                if(capability == IO_CAPABILITY) return IO_CAPABILITY.<T> cast(instance);
-                return null;
-            }
-        };
+        return BasicProvider.provide("left",true);
     }
 
     @SideOnly(Side.CLIENT)
