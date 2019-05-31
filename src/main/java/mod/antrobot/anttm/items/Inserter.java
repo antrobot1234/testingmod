da6565be7318cf4ea123a00e6015745943a40776
antrobot1234 <antrobot1234@gmail.com>
2019-05-30 22:40:42 -0500

    checking for client vs server methodized insert methods added chain capability maybe added custom textures

--- src/main/java/mod/antrobot/anttm/items/Inserter.java
+++ src/main/java/mod/antrobot/anttm/items/Inserter.java
@@ -1,6 +1,10 @@
 package mod.antrobot.anttm.items;
 
+import mod.antrobot.anttm.capabilities.basicio.BasicFactory;
 import mod.antrobot.anttm.capabilities.basicio.IBasicIO;
+import net.minecraft.client.renderer.ItemMeshDefinition;
+import net.minecraft.client.renderer.block.model.ModelBakery;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
@@ -13,9 +17,12 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityInject;
 import net.minecraftforge.common.capabilities.ICapabilitySerializable;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class Inserter extends Item{
 
@@ -30,15 +37,18 @@ public class Inserter extends Item{
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
     {
+        if(worldIn.isRemote) return new ActionResult<ItemStack>(EnumActionResult.PASS, playerIn.getHeldItem(handIn));
         ItemStack item = playerIn.getHeldItem(handIn);
         IBasicIO io = item.getCapability(IO_CAPABILITY,null);
         io.cycleInput();
-        playerIn.sendStatusMessage(new TextComponentString(io.getInput()),true);
+        playerIn.sendStatusMessage(new TextComponentString("pulling from: "+io.getInput()),true);
         return new ActionResult<ItemStack>(EnumActionResult.PASS, playerIn.getHeldItem(handIn));
     }
     @Override
     public void onUpdate(ItemStack stack, World world, Entity player, int slot, boolean held) {
         super.onUpdate(stack, world, player, slot, held);
+        if(world.isRemote)return;
+        if(world.getTotalWorldTime() % 20 != 0)return;
 
         IBasicIO io = stack.getCapability(IO_CAPABILITY,null);
 
@@ -49,8 +59,31 @@ public class Inserter extends Item{
 
         ItemStack inStack = inventory.getStackInSlot(inSlot);
         if (inStack.isEmpty()) return;
+        if (inStack.getItem()==this)return;
         int outSlot = io.calcOutput(slot);
         ItemStack outStack = inventory.getStackInSlot(outSlot);
+        if(outStack.getItem()==this){
+            forward(inventory,inStack,outStack,outSlot);
+        }
+        else{
+        insert(inventory,inStack,outStack,outSlot);
+        }
+    }
+    public void forward(InventoryPlayer inventory,ItemStack inStack, ItemStack currentWorker,int currentSlot){
+        if(inStack==currentWorker)return;
+        IBasicIO io = currentWorker.getCapability(IO_CAPABILITY,null);
+        if(io.isInvalid(currentSlot))return;
+        int outSlot = io.calcOutput(currentSlot);
+        ItemStack outStack = inventory.getStackInSlot(outSlot);
+        if(outStack.getItem()==this){
+            forward(inventory,inStack,outStack,outSlot);
+            return;
+        }
+        else{
+            insert(inventory,inStack,outStack,outSlot);
+        }
+    }
+    public void insert(InventoryPlayer inventory,ItemStack inStack, ItemStack outStack, int outSlot){
         if(!outStack.isEmpty()) {
             if (outStack.getCount() == outStack.getMaxStackSize()) return;
             if (!inStack.isItemEqual(outStack)) return;
@@ -61,9 +94,8 @@ public class Inserter extends Item{
         else{
             ItemStack stackClone = inStack.splitStack(1);
             inventory.setInventorySlotContents(outSlot,stackClone);
-            inventory.getStackInSlot(outSlot).setCount(1);
         }
-        ((EntityPlayer) player).inventoryContainer.detectAndSendChanges();
+        inventory.player.inventoryContainer.detectAndSendChanges();
     }
 
     @Override
@@ -74,7 +106,7 @@ public class Inserter extends Item{
             @Override
             public NBTTagCompound serializeNBT(){
                 NBTTagCompound nbt = new NBTTagCompound();
-                nbt.setString("input",instance.getInput());
+                nbt.setString("input",instance.getInput().toString());
                 nbt.setBoolean("output",instance.getOutput());
                 return nbt;
             }
@@ -96,5 +128,28 @@ public class Inserter extends Item{
                 return null;
             }
         };
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void initModel(){
+        ModelResourceLocation up = new ModelResourceLocation(getRegistryName()+"_up","inventory");
+        ModelResourceLocation left = new ModelResourceLocation(getRegistryName()+"_left","inventory");
+        ModelResourceLocation down = new ModelResourceLocation(getRegistryName()+"_down","inventory");
+        ModelResourceLocation right = new ModelResourceLocation(getRegistryName()+"_right","inventory");
+
+        ModelBakery.registerItemVariants(this,up,down,left,right);
+
+        ModelLoader.setCustomMeshDefinition(this,new ItemMeshDefinition(){
+            @Override
+            public ModelResourceLocation getModelLocation(ItemStack stack) {
+                BasicFactory.InputFacing input = stack.getCapability(IO_CAPABILITY,null).getInput();
+                switch(input){
+                    case up: return down;
+                    case down: return up;
+                    case right: return left;
+                    default: return right;
+                }
+            }
+        });
     }
 }
